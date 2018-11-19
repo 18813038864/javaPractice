f9a795f8a57d96160c37998b26fe25e4c887afe3
zengxinagji <zengxiangji@xdf.cn>
2018-11-19 17:17:13 +0800

    aspect

---
+++ src/main/java/aspectjDemo/Account.java
@@ -0,0 +1,13 @@
+package aspectjDemo;
+
+public class Account {
+    int balance = 20;
+
+    public boolean pay(int amount) {
+        if (balance < amount) {
+            return false;
+        }
+        balance -= amount;
+        return true;
+    }
+}
---
+++ src/main/java/aspectjDemo/Application.java
@@ -0,0 +1,14 @@
+package aspectjDemo;
+
+public class Application {
+    public static void main(String[] args) {
+        testCompileTime();
+    }
+    public static void testCompileTime() {
+        Account account = new Account();
+        System.out.println("==================");
+        account.pay(10);
+        account.pay(50);
+        System.out.println("==================");
+    }
+}
